7b6a504e3c3024aab4bf0c44aa1c1ca6690a3464
Lera <cvali7562@gmail.com>
2025-04-06 16:46:33 +0000

    ex3.cpp

---
+++ simple_tasks/ex3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+int main()
+{
+	int a, b;
+	std::cout << "enter two numbers ";
+	std::cin >> a >> b;
+	std::cout << "add" << (a+b) << std::endl;
+	std::cout << "subtraction" << (a-b) << std::endl;
+	std::cout << "multiplication" << (a*b) << std::endl;
+	return 0;
+
+
+}
