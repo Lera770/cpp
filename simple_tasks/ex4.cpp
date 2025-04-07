c785ab26ac7938a21c13cf46d07bfd7b2ff93cec
Lera <cvali7562@gmail.com>
2025-04-07 17:55:41 +0000

    ex4.cpp

--- simple_tasks/ex4.cpp
+++ simple_tasks/ex4.cpp
@@ -7,7 +7,7 @@ int main ()
 	double surface_area = 2*(A*B+A*C+B*C);
 	double volume_of_a_parallelepiped = (A*B*C);
 	std::cout <<"surface area" << surface_area << std::endl;
-	std::cout << "surface area" << volume_of_a_parallelepiped << std::endl;
+	std::cout << "volume of a parallelepiped" << volume_of_a_parallelepiped << std::endl;
 	return 0;
 
 
