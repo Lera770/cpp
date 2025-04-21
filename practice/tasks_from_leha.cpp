e612758c1eafed428ebc2b6fc2fab69b9c73686c
Lera <cvali7562@gmail.com>
2025-04-21 20:09:27 +0000

    tasks_from_leha.cpp

---
+++ practice/tasks_from_leha.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+int main()
+{
+	int a;
+	std::cout << "enter number";
+	std::cin >> a;
+	
+	for(int i = 0; i<a; i++)
+	{
+		std::cout << "Hello world!" << std::endl;
+	}
+
+	return 0;
+}
